fae3a7fbe16421b9eec7ecea2627b30dfbd7c4cd
GUSTAVO YAMAUCHI SOUZA REIS <gustavo.ysreis@senacsp.edu.br>
2022-02-24 21:40:53 -0300

    Cryptos

---
+++ cv/src/index.js
@@ -0,0 +1,40 @@
+import React, { useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+
+function Descricao(){
+  let [crypts, setCrypts] = React.useState(null)
+  
+  
+  useEffect(() => {
+    fetch("https://api.wazirx.com/sapi/v1/tickers/24hr")
+    .then(res => res.json())
+    .then(data => setCrypts(data))
+  }, [])
+
+
+  return (
+    !crypts ? <h1>Carregando</h1> : 
+    <>
+      <p>{crypts[0].bassedAssets}</p>
+    </>
+  )
+}
+
+
+
+
+
+
+ReactDOM.render(
+    <Descricao/>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
